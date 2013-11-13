8d613c6c61c9d010ed01b3a2348a51dbd9874d57
Eko <eko@eko-lenovo.(none)>
2013-11-13 20:13:11 +0200

    updated mail and library

--- library.rb
+++ library.rb
@@ -1,90 +1,122 @@
 class Library
-	attr_accessor :reader_name, :book, :order_date, :issue_date
-	def initialize(reader_name, book, order_date, issue_date)
-		@reader_name = reader_name
-		@book = book
-		@order_date = order_date
-		@issue_date = issue_date
+	@books = [] #book = {title: ""}
+
+	def initialize(books=[])
+		@books = books
+		@orders = []
 	end
 
-    class << self
-		def unsatisfied_orders(arr)
-			@count = 0
-			arr.each {|val| @count+= 1 if val.issue_date.nil?}
-			puts "#{@count} orders were not satisfied"
+  def order(reader_name, book_title)
+		finded_book = nil
+		@books.each do |book|
+			if book[:title] == book_title
+				finded_book = true 
+      if finded_book 
+    	issue_date = Time.now + rand(200..500000)
+    	else
+    		nil
+    	end
+		order = { reader_name: reader_name, book_title: book_title, order_date: Time.now, issue_date: issue_date, reader_name: reader_name}
+		@orders << order 
+	  end 
+  end
+
+	def unsatisfied_orders
+		count = 0
+		@orders.each {|order| count+= 1 if order[:issue_date].nil?}
+		puts "#{count} orders were not satisfied"
+	end
+
+	def fastest_find
+		count = 0
+		periods = []
+		@orders.each do |order|
+			periods << Time.at(order[:issue_date]-order[:order_date]).strftime("%d days") unless order[:issue_date].nil?
 		end
+		periods.sort {|x,y| y <=> x}
+		puts "Shortest period is: #{periods[0]}"
+	end
 
-#		def fastest_find(arr)
-#			@count = 0
-#			arr.each {|val| @count+= 1 if val.value > 10}
-#			puts "#{@count} parcels with value > 10 were sent"
-#		end
-
-		def who_takes_often(arr, book)
-			@count = 0
-			@i = 0
-			arr.each_with_index do |val, index|
-				if val.book == book
-				@temp_match = val.reader_name
-					while @i != index && @i < arr.length
-						if arr[@i].reader_name == @temp_match
-							@match = arr[@i].reader_name
-						end
-						@i+= 1
+	def who_takes_often(book_title)
+		i = 0
+		match = ''
+		@orders.each_with_index do |order, index|
+			if order[:book_title] == book_title
+				temp_match = order[:reader_name]
+				while i != index && i < @orders.length
+					if @orders[i][:reader_name] == temp_match
+						match = @orders[i][:reader_name]
 					end
+					i+= 1
 				end
 			end
-			if @match.empty?
-				puts "#{@temp_match} often takes '#{book}'" 
-				else puts "#{@match} often takes '#{book}'"
-			end
 		end
-
-		def popular_book(arr)
-			@count = 0
-			@i = 0
-			arr.each_with_index do |val, index|
-			@temp_match = val.book
-				while @i != index && @i < arr.length
-					if arr[@i].book == @temp_match
-						@match = arr[@i].book
-					end
-					@i+= 1
-				end
-			end
-			puts "The most popular book is '#{@match}'"
+		if match.empty?
+			puts "#{book_title} is nobody's favourite book"
+		else
+			puts "#{match} often takes #{book_title}"
 		end
+	end
 
-#		def one_of_three(arr)
-#			@count = 0
-#			@i = 0
-#			@match = Array.new
-#			arr.each_with_index do |val, index|
-#			@temp_match = val.book
-#				while @i != index && @i < arr.length
-#					if arr[@i].book == @temp_match
-#						while @i < 2
-#							@match[@i] = arr[@i].book
-#						end
-#					end
-#					@i+= 1
-#				end
-#			end
-#			puts "#{@count} people ordered #{@match}"
-#		end
- 	end
+	def one_of_three
+		index3 = 0
+		books_popularity = []
+		readers = []
+		i = 0
+		match = ''
+		@books.each do |book|
+			popularity = 0
+			@orders.each do |order|
+				popularity += 1 if order[:book_title] == book[:book_title]
+				reader_name = order[:reader_name]					
+			end
+			books_popularity << { book_title: book[:title], popularity: popularity}
+		end 
+		books_popularity.sort_by 
+		books_popularity.each_with_index do |book, index|
+			unless index > 2
+					@orders.each_with_index do |order, index2|
+				 		if order[:book_title] == book[:book_title]
+		 	 	 	  end
+		 	 	 	end
+		 	end
+	  end
+			puts "#{readers.uniq} often takes "
+	end
+
+	def popular_book
+		books_popularity = []
+		reader_name = []
+		@books.each do |book|
+			popularity = 0
+			@orders.each do |order|
+				popularity += 1 if order[:book_title] == book[:title]						
+			end
+			books_popularity << { book_title: book[:title], popularity: popularity}
+		end 
+		books_popularity.sort_by 
+		puts "The most popular book is '#{books_popularity[0][:book_title]}'"
+		puts books_popularity
+	end
 
 end
+end
 
-	data = Array.new
-	data[0] = Library.new("Ivan", "LOTR: The Fellowship of the Ring", "12.12.2012", nil)
-	data[1] = Library.new("Vasya", "War and Peace: part 1", "02.02.2002", "03.02.2002")
-	data[2] = Library.new("Ivan", "The Witcher", "13.12.2010", "15.12.2010")
-	data[3] = Library.new("Igor", "The Hunger Games", "29.12.2012", nil)
-	data[4] = Library.new("Ivan", "The Witcher", "13.11.2010", "15.11.2010")
-	data[5] = Library.new("Vladimir", "The Witcher", "05.12.2010", "06.12.2010")
-	data[6] = Library.new("Igor", "War and Peace: part 2", "13.12.2010", "15.12.2010")
+	library = Library.new([{title:"The Hunger Games"}, {title: "War and Peace: part 1"}, {title: "LOTR: The Fellowship of the Ring"}, {title: "The Witcher"}])
+	library.order("Ivan", "LOTR: The Fellowship of the Ring")
+	library.order("Vasya", "War and Peace: part 1")
+	library.order("Alexandr", "War and Peace: part 1")
+	library.order("Nina", "War and Peace: part 1")
+	library.order("Igor", "The Hunger Games")
+	library.order("Sergei", "The Hunger Games")
+	library.order("Ivan", "The Hunger Games")
+	library.order("Olya", "The Hunger Games")
+	library.order("Fedor", "The Hunger Games")
+	library.order("Fedor", "The Hunger Games")
+	library.order("Igor", "The Witcher")
 	
-	Library.unsatisfied_orders(data)
-	Library.popular_book(data)
-	Library.who_takes_often(data, "The Witcher")
+	library.unsatisfied_orders()
+	library.fastest_find()
+	library.popular_book()
+	library.who_takes_often("The Hunger Games")
+	library.one_of_three
--- mail.rb
+++ mail.rb
@@ -1,59 +1,58 @@
 class Mail
 	attr_accessor :city, :street, :house, :apartment, :destination, :value
-	def initialize(city, street, house, apartment, value)
-		@city = city
-		@street = street
-		@house = house
-		@apartment = apartment
-		@value = Integer(value)
-		@destination = String("City: " + city + ", street: " + street + ", house: " + house + ", ap: " + apartment)
+
+	def initialize 
+		@orders = []
 	end
 
-    class << self
-		def parcels_for_city(arr, city)
-			@count = 0
-			arr.each {|val| @count+= 1 if val.city == city}
-			puts "#{@count} parcels were sent to #{city} city"
+	def parcel(city, street, house, apartment, value)
+		order = {city: city, street: street, house: house, apartment: apartment, value: Integer(value), destination: String("City: " + city + ", street: " + street + ", house: " + house + ", ap: " + apartment)}
+		@orders << order
+	end 
+
+		def parcels_for_city(city)
+			count = 0
+			@orders.each {|val| count+= 1 if val[:city] == city}
+			puts "#{count} parcels were sent to #{city} city"
 		end
 
-		def parcels_with_value_higher_10(arr)
-			@count = 0
-			arr.each {|val| @count+= 1 if val.value > 10}
-			puts "#{@count} parcels with value > 10 were sent"
+		def parcels_with_value_higher_than(value)
+			count = 0
+			@orders.each {|val| count+= 1 if val[:value] > value}
+			puts "#{count} parcels with value > #{value} were sent"
 		end
 
-		def most_popular_address(arr)
-			@count = 0
-			@i = 0
-			arr.each_with_index do |val, index|
-			@temp_match = val.destination
-				while @i != index && @i < arr.length
-					if arr[@i].destination == @temp_match
-						@match = arr[@i].destination
+		def most_popular_address
+			i = 0
+			match = ''
+			@orders.each_with_index do |val, index|
+			temp_match = val[:destination]
+				while i != index && i < @orders.length
+					if @orders[i][:destination] == temp_match
+						match = @orders[i][:destination]
 					end
-					@i+= 1
+					i+= 1
 				end
 			end
-			if @match.empty?
-				puts "There is no populat address"
-				else puts "Most popular address: #{@match}"
+			if match.empty?
+				puts "There is no popular address"
+				else puts "Most popular address: #{match}"
 			end
 		end
- 	end
 
 end
 
-	data = Array.new
-	data[0] = Mail.new("Dnepr", "Glinki st.", "2", "11", 13)
-	data[1] = Mail.new("Dnepr", "Glinki st.", "2", "14", 2)
-	data[2] = Mail.new("Dnepr", "Glinki st.", "3", "15", 15)
-	data[3] = Mail.new("Kiyv", "Kirova st.", "5", "15", 10)
-	data[4] = Mail.new("Kiyv", "Lenina st.", "3", "25", 8)
-	data[5] = Mail.new("Kiyv", "Kirova st.", "5", "15", 19)
-	data[6] = Mail.new("Kiyv", "Kirova st.", "5", "15", 18)
-	data[7] = Mail.new("Kiyv", "Kirova st.", "5", "15", 18)
-	data[8] = Mail.new("Kiyv", "Kirova st.", "1", "11", 12)
+	order = Mail.new
+	order.parcel("Dnepr", "Glinki st.", "2", "11", 13)
+	order.parcel("Dnepr", "Glinki st.", "2", "14", 2)
+	order.parcel("Dnepr", "Glinki st.", "3", "15", 15)
+	order.parcel("Kiyv", "Kirova st.", "5", "15", 10)
+	order.parcel("Kiyv", "Lenina st.", "3", "25", 8)
+	order.parcel("Kiyv", "Kirova st.", "5", "15", 19)
+	order.parcel("Kiyv", "Kirova st.", "5", "15", 18)
+	order.parcel("Kiyv", "Kirova st.", "5", "15", 18)
+	order.parcel("Kiyv", "Kirova st.", "1", "11", 12)
 
-	Mail.parcels_for_city(data, "Dnepr")
-	Mail.parcels_with_value_higher_10(data)
-	Mail.most_popular_address(data)
+	order.parcels_for_city("Dnepr")
+	order.parcels_with_value_higher_than(10)
+	order.most_popular_address()
